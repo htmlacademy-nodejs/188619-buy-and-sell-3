4918e3bbd21aa251109c641ff3f26adc3f150d45
Bulat <garifb@gmail.com>
2021-07-24 09:06:53 +0300

    feat(express): base pug views

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -1,11 +1,13 @@
 'use strict';
 
-const Router = require(`express`);
+const {Router} = require(`express`);
 const mainRouter = new Router();
 
 mainRouter.get(`/`, (req, res) => res.render(`main`));
 mainRouter.get(`/register`, (req, res) => res.render(`sign-up`));
 mainRouter.get(`/login`, (req, res) => res.render(`login`));
 mainRouter.get(`/search`, (req, res) => res.render(`search-result`));
+mainRouter.get(`/404`, (req, res) => res.render(`errors/404`));
+mainRouter.get(`/500`, (req, res) => res.render(`errors/500`));
 
 module.exports = mainRouter;
--- src/express/routes/offers-routes.js
+++ src/express/routes/offers-routes.js
@@ -4,8 +4,8 @@ const {Router} = require(`express`);
 const offersRouter = new Router();
 
 offersRouter.get(`/category/:id`, (req, res) => res.render(`category`));
-offersRouter.get(`/add`, (req, res) => res.render(`/ticket/new-ticket`));
-offersRouter.get(`/edit/:id`, (req, res) => res.render(`/ticket/ticket-edit`));
-offersRouter.get(`/:id`, (req, res) => res.render(`/ticket/ticket`));
+offersRouter.get(`/add`, (req, res) => res.render(`ticket/new-ticket`));
+offersRouter.get(`/edit/:id`, (req, res) => res.render(`ticket/ticket-edit`));
+offersRouter.get(`/:id`, (req, res) => res.render(`ticket/ticket`));
 
 module.exports = offersRouter;
