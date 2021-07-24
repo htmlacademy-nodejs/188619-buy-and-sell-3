89f24ccc520deb1b4491f058ea9ec0278f2b6bdb
Bulat <garifb@gmail.com>
2021-07-24 19:48:29 +0300

    feat(cli): server command handled by express

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const http = require(`http`);
+const express = require(`express`);
 const fs = require(`fs`).promises;
 const chalk = require(`chalk`);
 const {HttpCode} = require(`../../constants`);
@@ -8,43 +8,8 @@ const {HttpCode} = require(`../../constants`);
 const DEFAULT_PORT = 3000;
 const FILENAME = `mocks.json`;
 
-const sendResponse = (res, statusCode, message) => {
-  const template = `
-    <!Doctype html>
-      <html lang="ru">
-      <head>
-        <title>With love from Node</title>
-      </head>
-      <body>${message}</body>
-    </html>`.trim();
-
-  res.writeHead(statusCode, {
-    'Content-Type': `text/html; charset=UTF-8`,
-  });
-
-  res.end(template);
-};
-
-const onClientConnect = async (req, res) => {
-  const notFoundMessageText = `Not found`;
-
-  switch (req.url) {
-    case `/`:
-      try {
-        const fileContent = await fs.readFile(FILENAME);
-        const mocks = JSON.parse(fileContent);
-        const message = mocks.map((post) => `<li>${post.title}</li>`).join(``);
-        sendResponse(res, HttpCode.OK, `<ul>${message}</ul>`);
-      } catch (err) {
-        sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
-      }
-
-      break;
-    default:
-      sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
-      break;
-  }
-};
+const app = express();
+app.use(express.json());
 
 module.exports = {
   name: `--server`,
@@ -52,14 +17,24 @@ module.exports = {
     const [customPort] = args;
     const port = Number.parseInt(customPort, 10) || DEFAULT_PORT;
 
-    http.createServer(onClientConnect)
-    .listen(port)
-    .on(`listening`, () => {
+    app.get(`/offers`, async (req, res) => {
+      try {
+        const fileContent = await fs.readFile(FILENAME);
+        const mocks = JSON.parse(fileContent);
+        res.json(mocks);
+      } catch (err) {
+        res.status(HttpCode.INTERNAL_SERVER_ERROR).send(err);
+      }
+    });
+
+    app.use((req, res) => res
+      .status(HttpCode.NOT_FOUND)
+      .send(`Not found`));
+
+    app.listen(port, () => {
       console.info(chalk.green(`Ожидаю соединений на ${port}`));
-    })
-    .on(`error`, ({message}) => {
-      console.error(chalk.red(`Ошибка при создании сервера: ${message}`));
+    }).on(`error`, (err) => {
+      console.error(chalk.red(`Ошибка при создании сервера: ${err}`));
     });
   }
 };
-
